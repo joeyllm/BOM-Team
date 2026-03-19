d796ca834fc7d3255c104f1fd33cc6f93b25eee7
Kecheng Zhang <kechengzhang28@gmail.com>
2026-03-19 11:51:54 +1100

    test animation

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -1,1 +1,8 @@
-import { animate } from 'https://esm.sh/animejs';
+import { animate, utils, createTimeline, createAnimatable, createDraggable } from 'https://esm.sh/animejs';
+
+animate('.square', {
+    width: '200px',
+    height: '200px',
+    backgroundColor: 'red',
+    easing: 'inOutQuad',
+});
