285480d7aa4ed06978ee90ea371d1397bd26d004
Kecheng Zhang <kechengzhang28@gmail.com>
2026-03-19 16:46:10 +1100

    use Three.js

--- docs/assets/script.js
+++
@@ -1,8 +0,0 @@
-import { animate, utils, createTimeline, createAnimatable, createDraggable } from 'https://esm.sh/animejs';
-
-animate('.square', {
-    width: '200px',
-    height: '200px',
-    backgroundColor: 'red',
-    easing: 'inOutQuad',
-});
