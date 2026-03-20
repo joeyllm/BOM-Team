49b4cacaf1ea3c41f26be2c71181c053055f542c
Kecheng Zhang <kechengzhang28@gmail.com>
2026-03-20 18:40:14 +1100

    update visual

--- docs/assets/background.js
+++ docs/assets/background.js
@@ -8,22 +8,26 @@ const freq = 64;
 const thresh = 0.6;
 const speed = 3;
 
+const size = 8.0; // change size here
+const spacing = 2; // change spacing here
+
 noise.seed(8715);
 
 // render function
 const vs = `#version 300 es
-in vec2 p; in float size; uniform float t; uniform float w; uniform float h; out vec3 c;
+in vec2 p; in float brightness; uniform float t; uniform float w; uniform float h; out float v_brightness;
 void main() {
     vec2 pos = p*2.0/vec2(w,h)-1.0;
     gl_Position = vec4(pos,0,1);
-    gl_PointSize = size;
+    gl_PointSize = ${size.toFixed(1)};
+    v_brightness = brightness;
 }`;
 
 const fs = `#version 300 es
-precision mediump float; in vec3 c; out vec4 f;
+precision mediump float; in float v_brightness; out vec4 f;
 void main() { 
     vec2 coord = gl_PointCoord-0.5;
-    f = vec4(0.15,0.25,0.45,1);
+    f = vec4(0.15 * v_brightness, 0.25 * v_brightness, 0.45 * v_brightness, 1);
 }`;
 
 // Compile shaders
@@ -35,8 +39,6 @@ const p = gl.createProgram();
 gl.linkProgram(p); gl.useProgram(p);
 
 // Generate point data (fixed spacing)
-const size = 3.0; // change size here
-const spacing = 8; // change spacing here
 let d = [], cols = 0, rows = 0;
 
 function generatePoints() {
@@ -44,9 +46,8 @@ function generatePoints() {
     cols = Math.ceil(innerWidth / length) + 1;
     rows = Math.ceil(innerHeight / length) + 1;
     d = [];
-    var thisSize = size;
     for(let y=0;y<rows;y++) for(let x=0;x<cols;x++) {
-        d.push(x*length,y*length,thisSize,x,y);
+        d.push(x*length,y*length,1.0,x,y);
     }
 }
 
@@ -60,9 +61,9 @@ const a = gl.getAttribLocation(p,'p');
 gl.enableVertexAttribArray(a);
 gl.vertexAttribPointer(a,2,gl.FLOAT,false,20,0);
 
-const sizeLoc = gl.getAttribLocation(p, 'size');
-gl.enableVertexAttribArray(sizeLoc);
-gl.vertexAttribPointer(sizeLoc, 1, gl.FLOAT, false, 20, 8);
+const brightnessLoc = gl.getAttribLocation(p, 'brightness');
+gl.enableVertexAttribArray(brightnessLoc);
+gl.vertexAttribPointer(brightnessLoc, 1, gl.FLOAT, false, 20, 8);
 
 function updateBuffer() {
     gl.bindBuffer(gl.ARRAY_BUFFER,b);
@@ -111,13 +112,8 @@ function render(t) {
         let n2 = curlNoise(gridX/freq, gridY/freq, t * 0.7);
         let scaledN = (n + 0.2 * n2) * 4;
         let prob = sigmoid(scaledN);
-        let value = 0;
-        if (n2 > thresh) {
-            value = size * prob;
-        } else {
-            value = size * prob * 0.5;
-        };
-        d[i*5+2] = prob * size; 
+        let brightness = prob;
+        d[i*5+2] = brightness;
     }
     updateBuffer();
     
